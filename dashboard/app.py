54092912e07bddb2cf3afe7df02e1cff2e4879c3
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2026-06-11 16:30:30 +0200

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -13,3 +13,18 @@ with open("dashboard/models/best_model.pkl", "rb") as f:
 st.success("App loaded correctly")
 st.write(type(model))
 #---------------------------------------------------------
+input_data = [[
+    5,      # vehicle_age
+    120000, # km_driven
+    90,     # engine_temp
+    0.3,    # vibration_level
+    0.8,    # battery_health
+    5000,   # last_service_km
+    10,     # maintenance_delay_days
+    6,      # driving_hours_day
+    3000    # load_weight
+]]
+
+prediction = model.predict(input_data)
+
+st.write("Failure risk:", prediction[0])
