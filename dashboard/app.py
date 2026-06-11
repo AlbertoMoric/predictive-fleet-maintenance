76a9f59b649f0fed00308cddba5ae873efbf8839
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2026-06-11 16:17:36 +0200

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,3 +6,6 @@ import pickle
 st.title("Predictive Fleet Maintenance")
 # cargar dataset
 df = pd.read_csv("dashboard/data/fleet_dataset.csv")
+import pickle
+with open("dashboard/models/best_model.pkl", "rb") as f:
+    model = pickle.load(f)
