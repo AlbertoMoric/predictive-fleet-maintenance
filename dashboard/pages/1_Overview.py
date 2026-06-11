e3c508bd8c313a41adba526de47a8e4ec8862cb0
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2026-06-11 23:54:26 +0200

    Update 1_Overview.py

--- dashboard/pages/1_Overview.py
+++ dashboard/pages/1_Overview.py
@@ -1,1 +1,13 @@
-
+import streamlit as st
+import pandas as pd
+# dataset
+df = pd.read_csv("dashboard/data/fleet_dataset.csv")
+# -----------------------------
+# HERO SECTION
+# -----------------------------
+st.title("🚛 Predictive Fleet Maintenance")
+st.markdown("""
+Welcome to the fleet monitoring dashboard.
+Monitor vehicle health, detect potential failures,
+and analyze fleet performance in real time.
+""")
