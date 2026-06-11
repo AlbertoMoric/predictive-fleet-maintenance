c34bd34914889b40a94cdc30973ad9a4841cd332
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2026-06-11 14:49:43 +0200

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import pickle
 
+# título
+st.title("Predictive Fleet Maintenance")
+# cargar dataset
+df = pd.read_csv("data/fleet_dataset.csv")
+# cargar modelo
+with open("models/best_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+st.success("System loaded successfully")
+st.write(df.head())
