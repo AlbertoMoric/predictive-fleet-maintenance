b4f34d70c837fc07252ced7b1f1a84ce51124eea
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2026-06-11 16:29:51 +0200

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-
+#---------------------------------------------------------
 # título
 st.title("Predictive Fleet Maintenance")
 # cargar dataset
@@ -9,7 +9,7 @@ df = pd.read_csv("dashboard/data/fleet_dataset.csv")
 # cargar modelo
 import pickle
 with open("dashboard/models/best_model.pkl", "rb") as f:
-    model = pickle.load(f)
-    
+    model = pickle.load(f)    
 st.success("App loaded correctly")
 st.write(type(model))
+#---------------------------------------------------------
